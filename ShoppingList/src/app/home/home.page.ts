e56121f81664397fd9d0278563a5fbfed899a888
CapiHidalgo <alvaro.hidalgo@ununuzi.es>
2019-11-05 11:42:23 +0100

    save in the storage the shopping list

--- ShoppingList/src/app/home/home.page.ts
+++ ShoppingList/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +12,29 @@ export class HomePage {
   itemName: string;
   constructor(
     public navCtrl: NavController,
-    public alertCtrl: AlertController
-  ) {}
+    public alertCtrl: AlertController,
+    private storage: Storage
+  ) {
+    this.storage.ready().then(
+      () => this.storage.get('productList').then(
+        (value) => this.shoppingList = JSON.parse(value)
+      )
+    );
+  }
 
-  addItem() {
+  async addItem() {
     if (this.shoppingList.length >= 0) {
       let item = this.itemName;
       console.log(item);
       this.shoppingList.push(item);
       this.itemName = '';
+      await this.storage.set('productList', JSON.stringify(this.shoppingList));
     }
   }
 
   deleteItem(index) {
     this.shoppingList.splice(index, 1);
+    this.storage.set('productList', JSON.stringify(this.shoppingList));
   }
 
   async updateItem(index) {
@@ -33,7 +43,9 @@ export class HomePage {
       inputs: [{ name: 'editItem', placeholder: 'Item'}],
       buttons: [{ text: 'Cancel', role: 'cancel' },
                 {text: 'update', handler: data => {
-                  this.shoppingList[index] = data.editItem; }
+                  this.shoppingList[index] = data.editItem;
+                  this.storage.set('productList', JSON.stringify(this.shoppingList));
+                 }
                 }
               ]
             });
@@ -42,6 +54,7 @@ export class HomePage {
 
   deleteAll(){
     this.shoppingList = [];
+    this.storage.set('productList', JSON.stringify(this.shoppingList));
   }
 
 }
